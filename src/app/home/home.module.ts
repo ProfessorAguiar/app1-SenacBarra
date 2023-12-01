e2bbe63bc1c71027b33e0c5ebdf0efce3cc522fc
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-12-01 15:46:54 -0300

    App ionic em blank template

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { CardComponent } from '../Components/card/card.component';
 
 @NgModule({
   imports: [
@@ -14,6 +14,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, CardComponent]
 })
 export class HomePageModule {}
